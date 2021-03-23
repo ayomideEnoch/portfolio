7d265535ec9c5a227018e42d029c40a549384667
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-03-23 15:19:41 +0100

    Iterating...

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="bg-black min-h-screen w-full">
+  <div class="min-h-screen w-full bg-b" style="background-color: #151618">
     <section class="flex justify-between px-24 pt-8 text-gray-300">
       <div>
-        <button class="flex items-center">
+        <button class="flex items-center focus:outline-none">
           <img class="w-16" src="./assets/AYOMIDE.webp" alt="" />
           <div class="font-bold cursor pl-5">AYOMIDE IBOSIOLA</div>
         </button>
       </div>
 
       <div class="flex space-x-12">
-        <button class="font-bold">ABOUT</button>
-        <button class="font-bold">PORTFOLIO</button>
-        <button class="font-bold">CONTACT</button>
+        <button class="font-bold focus:outline-none">ABOUT</button>
+        <button class="font-bold focus:outline-none">PORTFOLIO</button>
+        <button class="font-bold focus:outline-none">CONTACT</button>
       </div>
     </section>
+
     <section>
       <div class="flex items-center justify-center mt-12">
         <div class="justify-left">
-          <div class="text-white">
-            <button><i class="fa fa-facebook"></i></button>
-            <button><i class="fas fa-twitter"></i></button>
-            <button><i class="fa fa-github"></i></button>
-            <button><i class="fa fa -linkedin"></i></button>
+          <div class="cursor-pointer space-y-4 -mx-40 px-2">
+            <div
+              class="text-xs font-semibold text-black bg-white rounded-full p-1 items-center justify-center w-4 flex h-4"
+            >
+              <i class="fab fa-facebook-f"></i>
+            </div>
+
+            <div
+              class="text-xs font-semibold text-black bg-white rounded-full p-1 justify-center items-center w-4 flex h-4"
+            >
+              <i class="fab fa-twitter"></i>
+            </div>
+
+            <div
+              class="text-xs font-semibold text-black bg-white rounded-full p-1 justify-center items-center w-4 flex h-4"
+            >
+              <i class="fab fa-github"></i>
+            </div>
+
+            <div
+              class="text-xs font-semibold text-black bg-white rounded-full p-1 justify-center items-center w-4 flex h-4"
+            >
+              <i class="fab fa-linkedin-in"></i>
+            </div>
           </div>
         </div>
         <div class="">
@@ -36,11 +56,14 @@
         </div>
       </div>
     </section>
-    <section class="flex justify-between px-24 -mt-4 text-gray-300 font-bold">
+    <section
+      class="flex justify-between px-24 -mt-4 text-gray-300 font-bold cursor-pointer"
+    >
       <div class="text-8xl"><p>01</p></div>
-      <div class="">
-        <p class="text-sm text-bold">SCROLL DOWN</p>
-        <i class="fa fa-angle-down"></i>
+      <div class="transform rotate-90">
+        <p class="text-sm text-bold">
+          SCROLL DOWN <i class="fas fa-arrow-alt-right"></i>
+        </p>
       </div>
     </section>
   </div>
