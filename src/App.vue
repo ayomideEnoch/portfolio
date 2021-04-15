9833040bcae9b71df45b924d26c592eb638977d3
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-04-15 12:20:01 +0100

    Iterating...

--- src/App.vue
+++ src/App.vue
@@ -283,7 +283,7 @@
             >
               <p class="text-2xl font-semibold">{{ portfolio.title }}</p>
 
-              <p class="text-xl">{{ portfolio.description }}</p>
+              <p class="text-sm">{{ portfolio.description }}</p>
               <a :href="portfolio.link" class="absolute bottom-2 -ml-14 pl-0.5">
                 <button class="bg-black px-3 py-2 rounded-md cursor-pointer text-white">
                   Click Here
